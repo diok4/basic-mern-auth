c932c90d2e14613d2bfb972eea933082ced6305c
diok4 <webdeveloperpath21@gmail.com>
2025-09-20 13:21:17 +0500

    updated login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -79,6 +79,7 @@ export const login = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
