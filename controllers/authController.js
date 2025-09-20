32b9dd45e67c5f4f9f8a4c15fb2af154bd94f5f1
diok4 <webdeveloperpath21@gmail.com>
2025-09-20 13:16:58 +0500

    updated logout logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -93,6 +93,7 @@ export const logout = (req, res) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    path: "/", // обязательно!
   });
   res.json({ success: true, message: "Logged out successfully" });
 };
